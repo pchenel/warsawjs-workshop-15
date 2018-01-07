34c2a41a7cf29ecb4f29895ddf295987a99a67d4
Pchenel <pchenel@gmail.com>
2018-01-07 12:19:41 +0100

    trzeci krok

--- game.js
+++ game.js
@@ -1,21 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
+//zadeklarowanie obiektow (jest dwoch graczy i kazdy mam swoj kolor
+    var playerClasses = {
+        'playerA': 'red',
+        'playerB': 'blue'
+    };
+    var currentPlayer;
+
    initGame();
    function initGame() {
        var fields = document.querySelectorAll('.board > div');
+//zaczynamy od graczaA
+       currentPlayer = 'playerA';
+
        fields.forEach(field => {
            field.addEventListener('click', fieldClickHandler);
        });
    }
    function fieldClickHandler () {
-       console.log('clicked', this);
-       this.classList.add('red');
+       //deklarujemy zmienna i ustalamy jej wartosc poczatkowoa
+       var playerClass = playerClasses[currentPlayer];
+       //dodajemy klase by kolor nam sie zmienil
+       this.classList.add(playerClass);
+       //tu mamy w skrocie instrukcje if switch
+       currentPlayer = currentPlayer === 'playerA' ? 'playerB' : 'playerA';
    }
 });
 
-
-
-
-
+//instrukcja if switch - czyli jesli mam jednego gracza to zamieniam na drugiego a jesli mam drugiego to zmieniam na pierwszego)
+// if (currentPlayer === PlayerA) {
+//      currentPlayer = PlayerB;
+//}
+// if (currentPlayer === PlayerB) {
+//      currentPlayer = PlayerA;
+//}
+//lub
+// if (currentPlayer === PlayerB) {
+//      currentPlayer = PlayerA;
+//} else (currentPlayer = PlayerB);
+//lub
+//currentPlayer === 'playerA' ? currentPlayer = 'playerB' : currentPlayer = 'playerA'
+//lub
+//currentPlayer = currentPlayer === 'playerA' ? 'playerB' : 'playerA';
 
 
 
