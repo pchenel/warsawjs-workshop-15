bcd5dffd278ac229338ffb3013c126ce473e5cd9
Pchenel <pchenel@gmail.com>
2018-01-07 14:09:10 +0100

    krok szosty tablica

--- game.js
+++ game.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
 //zadeklarowanie obiektow (jest dwoch graczy i kazdy mam swoj kolor
+    var fields = document.querySelectorAll('.board > div');
     var playerClasses = {
         'playerA': 'red',
         'playerB': 'blue'
     };
+    var array
     var currentPlayer;
     var emptyFields;
 
     initGame();
+
     function initGame() {
-        var fields = document.querySelectorAll('.board > div');
+
 //zaczynamy od graczaA
         currentPlayer = 'playerA';
         emptyFields = 9;
@@ -30,11 +33,36 @@ document.addEventListener('DOMContentLoaded', function () {
         currentPlayer = currentPlayer === 'playerA' ? 'playerB' : 'playerA';
         this.removeEventListener('click', fieldClickHandler);
         emptyFields -= 1;
-        if (emptyFields === 0) {
-            alert('Koniec gry. Wygral ' + currentPlayer);
-        }
+
+        checkWinner();
+
 
     }
+
+    function checkWinner() {
+        var winning = [
+            fields[0].className + fields[1].className + fields[2].className,
+            fields[3].className + fields[4].className + fields[5].className,
+            fields[6].className + fields[7].className + fields[8].className,
+            fields[0].className + fields[3].className + fields[6].className,
+            fields[1].className + fields[4].className + fields[7].className,
+            fields[2].className + fields[5].className + fields[8].className,
+            fields[0].className + fields[4].className + fields[8].className,
+            fields[2].className + fields[4].className + fields[6].className
+        ];
+
+
+        if (winning.includes('redredred')) {
+            alert('Game over. PlayerA win.');
+        } else if
+        (winning.includes ('blueblueblue')) {
+            alert('Game over. PlayerB win.');
+        } else if (emptyFields === 0) {
+                alert('Game over. No one win.');
+            }
+        }
+
+
 });
 
 //instrukcja if switch - czyli jesli mam jednego gracza to zamieniam na drugiego a jesli mam drugiego to zmieniam na pierwszego)
