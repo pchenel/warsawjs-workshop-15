1cfd59d5134d73fb839e5f4f44380e89012d1743
Pchenel <pchenel@gmail.com>
2018-01-07 12:35:22 +0100

    krok piąty

--- game.js
+++ game.js
@@ -1,31 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 //zadeklarowanie obiektow (jest dwoch graczy i kazdy mam swoj kolor
     var playerClasses = {
         'playerA': 'red',
         'playerB': 'blue'
     };
     var currentPlayer;
+    var emptyFields;
 
-   initGame();
-   function initGame() {
-       var fields = document.querySelectorAll('.board > div');
+    initGame();
+    function initGame() {
+        var fields = document.querySelectorAll('.board > div');
 //zaczynamy od graczaA
-       currentPlayer = 'playerA';
+        currentPlayer = 'playerA';
+        emptyFields = 9;
 
-       fields.forEach(field => {
-           field.addEventListener('click', fieldClickHandler);
-       });
-   }
-   function fieldClickHandler () {
-       //deklarujemy zmienna i ustalamy jej wartosc poczatkowoa
-       var playerClass = playerClasses[currentPlayer];
-       //dodajemy klase by kolor nam sie zmienil
-       this.classList.add(playerClass);
-       //tu mamy w skrocie instrukcje if switch
-       currentPlayer = currentPlayer === 'playerA' ? 'playerB' : 'playerA';
-       this.removeEventListener('click', fieldClickHandler);
+        fields.forEach(field => {
+            field.addEventListener('click', fieldClickHandler);
 
-   }
+    })
+        ;
+    }
+
+    function fieldClickHandler() {
+        //deklarujemy zmienna i ustalamy jej wartosc poczatkowoa
+        var playerClass = playerClasses[currentPlayer];
+        //dodajemy klase by kolor nam sie zmienil
+        this.classList.add(playerClass);
+        //tu mamy w skrocie instrukcje if switch
+        currentPlayer = currentPlayer === 'playerA' ? 'playerB' : 'playerA';
+        this.removeEventListener('click', fieldClickHandler);
+        emptyFields -= 1;
+        if (emptyFields === 0) {
+            alert('Koniec gry. Wygral ' + currentPlayer);
+        }
+
+    }
 });
 
 //instrukcja if switch - czyli jesli mam jednego gracza to zamieniam na drugiego a jesli mam drugiego to zmieniam na pierwszego)
